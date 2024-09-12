1e2e9c0898a1570cd64e8ec6dc9de7da3a279555
Hyperspace146 <adriandinh146@gmail.com>
2024-09-12 14:21:44 -0700

    Started basic react app using create-react-app

---
+++ app/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>GAMER SUPERSCORE</h1>
+      </header>
+
+      <body className="App-body">
+        <p className="App-item">
+          hello
+        </p>
+      </body>
+    </div>
+  );
+}
+
+export default App;
